35394ef14b664ff69ac208a0ccb76e4906f8afca
Jozhué Beni <jbeeniif@gmail.com>
2024-01-31 16:29:35 -0300

    Finish Section Schedule

--- login-special-page/src/components/home/AboutUs/AboutUs.tsx
+++ login-special-page/src/components/home/AboutUs/AboutUs.tsx
@@ -1,44 +1,61 @@
-import { images } from "../../../hooks";
 import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div
-    className="app__aboutus app__bg flex__center section__padding"
-    id="about"
-  >
-    <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="G_overlay" />
+  <div className="about-container">
+    <div className="title-about">
+      <h3>SCHEDULE</h3>
+      <h5>our Restaurant</h5>
     </div>
+    <div className="align-general">
+      <img
+        className="img-div-2"
+        src="https://images.pexels.com/photos/2679501/pexels-photo-2679501.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt=""
+      />
 
-    <div className="app__aboutus-content flex__center">
-      <div className="app__aboutus-content_about">
-        <h1 className="headtext__cormorant">About Us</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
-          adipiscing ultrices vulputate posuere tristique. In sed odio nec
-          aliquet eu proin mauris et.
-        </p>
-        <button type="button" className="custom__button">
-          Know More
-        </button>
+      <div className="about-text">
+        <div className="hours-area">
+          <div>
+            <span>Monday -</span>
+            <span>12pm - 9pm</span>
+          </div>
+          <div>
+            <span>Tuesday -</span>
+            <span>12pm - 9pm</span>
+          </div>
+          <div>
+            <span>Wednesday -</span>
+            <span>12pm - 9pm</span>
+          </div>
+          <div>
+            <span>Thursday -</span>
+            <span>12pm - 9pm</span>
+          </div>
+          <div>
+            <span>Friday -</span>
+            <span>12pm - 11pm</span>
+          </div>
+          <div>
+            <span>Saturday -</span>
+            <span>12pm - 9pm</span>
+          </div>
+          <div>
+            <span>Sunday-</span>
+            <span>12pm - 9pm</span>
+          </div>
+        </div>
+
+        <div className="phone-area">
+          <h4>PHONE</h4>
+          <p>219.542.1432</p>
+          <p>219.777.1532</p>
+        </div>
       </div>
-
-      <div className="app__aboutus-content_knife flex__center">
-        <img src={images.knife} alt="about_knife" />
-      </div>
-
-      <div className="app__aboutus-content_history">
-        <h1 className="headtext__cormorant">Our History</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">
-          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
-          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
-          odio nec aliquet.
-        </p>
-        <button type="button" className="custom__button">
-          Know More
-        </button>
+      <div className="img-div-1">
+        <img
+          src="https://images.pexels.com/photos/5768972/pexels-photo-5768972.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
       </div>
     </div>
   </div>
