a52f962c74478bbf6ea50f47c1a752e6a2dcf778
Jozhué Beni <jbeeniif@gmail.com>
2024-01-31 14:37:10 -0300

    finish headerMenu

--- login-special-page/src/components/home/Header/Header.tsx
+++ login-special-page/src/components/home/Header/Header.tsx
@@ -1,21 +1,27 @@
-import { images } from "../../../hooks";
 import "./Header.css";
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <h1 className="app__header-h1">The flavors that you love the most</h1>
+      <h1 className="app__header-h1">
+        The <span>flavors</span> that you love the most
+      </h1>
       <p className="p__opensans" style={{ margin: "2rem 0" }}>
         We have 20 years of tradition, building a customer base that always
         praises us for our services.
       </p>
       <button type="button" className="custom__button">
-        Explore Menu
+        <a href="#menu">Daily menu</a>
       </button>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
+      <img
+        src={
+          "https://images.pexels.com/photos/541216/pexels-photo-541216.jpeg?cs=srgb&dl=pexels-flo-dahm-541216.jpg&fm=jpg"
+        }
+        alt="header_img"
+      />
     </div>
   </div>
 );
